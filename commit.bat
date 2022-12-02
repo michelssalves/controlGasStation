bbb61c2ec6fe5c3b79c6ded5a037ffbcc34be099
Michel <michelssalves@gmail.com>
2022-12-02 09:23:49 -0300

    novamente alterado -> 02-12-2022 -> Michel

--- commit.bat
+++ commit.bat
@@ -6,5 +6,5 @@ set filedatetime=%filedatetime:~0,2%-%filedatetime:~3,2%-%filedatetime:~6,4%
 
 set /p message="Digite o comentário do commit: "
 git add -A
-git commit -am "%message% %filedatetime%%username%"
+git commit -am "%message% -> %filedatetime% -> %username%"
 git push -u origin main
