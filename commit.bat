b151dc65f3e0723bc8c0151d1c9e714109fac837
Michel <michelssalves@gmail.com>
2022-12-02 09:20:18 -0300

    nova 02-12-2022

--- commit.bat
+++ commit.bat
@@ -1,8 +1,8 @@
 @echo off
 
 REM defina a data abaixo
-set filedatetime=%date%
-set filedatetime=%filedatetime:~0,2%%filedatetime:~3,2%%filedatetime:~6,4%
+set filedatetime=%date%-
+set filedatetime=%filedatetime:~0,2%-%filedatetime:~3,2%-%filedatetime:~6,4%
 
 set /p message="Digite o comentário do commit: "
 git add -A
