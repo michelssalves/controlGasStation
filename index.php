2b104dd67313f32ea9b921f9c44c7f04d99df7d7
Michel <michelssalves@gmail.com>
2022-12-05 10:21:13 -0300

    excl scripts Ref: 05-12-2022 - 10:21 -> Michel

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <!-- Font-Awesome -->
     <link rel="stylesheet" type="text/css" href="assets/css/fontawesome.all.min.6.2.1.css">
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <!-- W3 -->
 </head>
 
