6785226f0c295c78d7793c5e8a2e4565976761dc
Michel <michelssalves@gmail.com>
2023-01-03 14:04:41 -0300

    ajustado visualizaçao de documentos Ref: 03-01-2023 - 14:04 -> Michel

--- view/modal/visualizarDocumentosModal.view.php
+++ view/modal/visualizarDocumentosModal.view.php
@@ -1,70 +1,74 @@
 <form method="POST">
   <?php
   session_start();
+  if ($_REQUEST['doc'] <> '') {
+    $id_requisicao = $_REQUEST['id_requisicao'];
+    $doc = $_REQUEST['doc'];
+    $pasta = $_REQUEST['pasta'];
+    $descricao = $_REQUEST['descricao'];
+    $pasta = $_REQUEST['pasta'];
 
-  $id_requisicao = $_REQUEST['id_requisicao'];
-  $doc = $_REQUEST['doc'];
-  $pasta = $_REQUEST['pasta'];
-  $descricao = $_REQUEST['descricao'];
-  $pasta = $_REQUEST['pasta'];
+    list($idArquivo, $extensao) = explode(".", $doc);
 
-  list($idArquivo, $extensao) = explode(".", $doc);
-
-  //$raiz = "../../assets/docs/".$pasta."/".$doc; original
-  $raiz = "../../../medweb/fechamentoCaixa/docs/" . $doc;
-  $raiz2 = "../../assets/docs/FechamentoCaixa/367539.pdf";
+    //$raiz = "../../assets/docs/".$pasta."/".$doc; original
+    $raiz = "../../../medweb/fechamentoCaixa/docs/" . $doc;
+    $raiz2 = "../../assets/docs/FechamentoCaixa/367539.pdf";
   ?>
 
-  <head>
-    <link rel="stylesheet" type="text/css" href="../../assets/css/bootstrap.min.css" rel="stylesheet">
-  </head>
+    <head>
+      <link rel="stylesheet" type="text/css" href="../../assets/css/bootstrap.min.css" rel="stylesheet">
+    </head>
 
-  <body>
-    <div class="container">
-      <div class="row justify-content-md-center">
-        <div class="col-md-auto ">
-          <button value="excluirAnexo" name="action" type="submit" class="btn btn-danger btn-lg">Excluir</button>
+    <body>
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col-md-auto ">
+            <button value="excluirAnexo" name="action" type="submit" class="btn btn-danger btn-lg">Excluir</button>
+          </div>
         </div>
       </div>
-    </div>
-    <input type="hidden" name="raiz2" value="<?= $raiz2 ?>">
-    <input type="hidden" name="id_requisicao" value="<?= $id_requisicao ?>">
-    <input type="hidden" name="idArquivo" value="<?= $idArquivo ?>">
-    <input type="hidden" name="extensao" value="<?= $extensao ?>">
-    <input type="hidden" name="descricao" value="<?= $descricao ?>">
+      <input type="hidden" name="raiz2" value="<?= $raiz2 ?>">
+      <input type="hidden" name="id_requisicao" value="<?= $id_requisicao ?>">
+      <input type="hidden" name="idArquivo" value="<?= $idArquivo ?>">
+      <input type="hidden" name="extensao" value="<?= $extensao ?>">
+      <input type="hidden" name="descricao" value="<?= $descricao ?>">
 
-    <form>
-      <?php if ($extensao == 'pdf' || $extensao == 'PDF') { ?>
-        <embed src="<?= $raiz ?>" width="100%" height="100%" />
-      <?php } else { ?>
-        <iframe src="<?= $raiz ?>" width="100%" height="100%" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');">
-        </iframe>
-      <?php } ?>
-      <script src="../../assets/js/bootstrap.bundle.min.v5.2.3.js"></script>
-  </body>
+      <form>
+        <?php if ($extensao == 'pdf' || $extensao == 'PDF') { ?>
+          <embed src="<?= $raiz ?>" width="100%" height="100%" />
+        <?php } else { ?>
+          <iframe src="<?= $raiz ?>" width="100%" height="100%" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');">
+          </iframe>
+        <?php } ?>
+        <script src="../../assets/js/bootstrap.bundle.min.v5.2.3.js"></script>
+    </body>
   <?php
 
-  $action = $_REQUEST['action'];
+    $action = $_REQUEST['action'];
 
-  if ($action == 'excluirAnexo') {
+    if ($action == 'excluirAnexo') {
 
-    include('../../controller/controllerAux/validaLogin.php');
-    include('../../model/CaixaDiario.php');
+      include('../../controller/controllerAux/validaLogin.php');
+      include('../../model/CaixaDiario.php');
 
-    $raiz2 = $_REQUEST['raiz2'];
-    $idArquivo = $_REQUEST['idArquivo'];
-    $extensao = $_REQUEST['extensao'];
-    $doc = $_REQUEST['doc'];
-    $descricao = $_REQUEST['descricao'];
-    $id_requisicao = $_REQUEST['id_requisicao'];
-    $usuarioLogado;
-    $obs = "EXCLUIU UM ANEXO: $descricao - $doc";
+      $raiz2 = $_REQUEST['raiz2'];
+      $idArquivo = $_REQUEST['idArquivo'];
+      $extensao = $_REQUEST['extensao'];
+      $doc = $_REQUEST['doc'];
+      $descricao = $_REQUEST['descricao'];
+      $id_requisicao = $_REQUEST['id_requisicao'];
+      $usuarioLogado;
+      $obs = "EXCLUIU UM ANEXO: $descricao - $doc";
 
-    echo deleteFechamentoCaixaAnexo($idArquivo);
+      echo deleteFechamentoCaixaAnexo($idArquivo);
 
-    echo insertFechamentoCaixaObservacao($id_requisicao, $usuarioLogado, $obs);
+      echo insertFechamentoCaixaObservacao($id_requisicao, $usuarioLogado, $obs);
 
-    unlink($raiz2);
+      //unlink($raiz2);
+
+      header("Location: documentoExcluido.php");
+    }
+  } else {
+    header("Location: documentoExcluido.php");
   }
-
   ?>
